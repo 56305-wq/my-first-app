1f0a456442950db161cb5e28d8e4375d54ae2b93
417-16-sirawit <56305@yupparaj.ac.th>
2026-08-26 11:32:19 +0700

    Update vat_app.py

--- vat_app.py
+++ vat_app.py
@@ -6,4 +6,4 @@ net_price = price - vat
 st.header(f"• ภาษีมูลค่าเพิ่ม (VAT 7%): **{vat:.2f}** บาท")
 st.header(f"• ราคาสุทธิ: {net_price:.2f} บาท")
 st.divider()
-st.write("นางสาวดีใจ ยิ้มแย้ม เลขที่ 5  ม.4/5")
+st.write("นาย สิรวิชญ์ จอมนงค์ 16  ม.4/17")
