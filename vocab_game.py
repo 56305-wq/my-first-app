e21ec2a88fd4faa64c3c9cf9e62409f7ecdb1595
417-16-sirawit <56305@yupparaj.ac.th>
2026-09-09 11:33:19 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -102,6 +102,6 @@ if st.session_state.get("is_ended", False):
     show_result_dialog(ans1, ans2)
 
 st.divider()
-st.write("นางสาวดีใจ ยิ้มแย้ม เลขที่ 5 ม.4/5")
+st.write("นาย สิรวิชญ์ จอมนงค์ เลขที่ 16 ม.4/17")
 
 
