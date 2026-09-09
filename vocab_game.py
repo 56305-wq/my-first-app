36e8c7eacf7b759a8f39c09da2176cdee17a92cc
417-16-sirawit <56305@yupparaj.ac.th>
2026-09-09 11:55:42 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -52,13 +52,13 @@ def show_result_dialog(ans1, ans2, ans3, ans4):
         st.error(f"❌ ข้อ 2: ยังไม่ถูกต้อง (คุณตอบ '{u_ans2}')")
      
     # ตรวจข้อ 3
-     if u_ans1 == "dog":
+     if u_ans3 == "dog":
         st.success("✅ ข้อ 3: ถูกต้อง")
         score += 1
     else:
         st.error(f"❌ ข้อ 3: ยังไม่ถูกต้อง (คุณตอบ '{u_ans3}')")
     # ตรวจข้อ 4
-    if u_ans1 == "pillow":
+    if u_ans4 == "pillow":
         st.success("✅ ข้อ 4: ถูกต้อง")
         score += 1
     else:
