a133a1130b435034101def90d4c4ea80c9d75410
417-16-sirawit <56305@yupparaj.ac.th>
2026-09-16 11:46:25 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -67,8 +67,7 @@ if u_ans4 == "pillow":
     score += 1
 else:
     st.error(f"❌ ข้อ 4: ยังไม่ถูกต้อง (คุณตอบ '{u_ans4}')")
-
-   score = 4
+    score = 4
    st.info(f"🏆 ได้คะแนนรวม:{score} คะแนน")
 
     if score == 4:
